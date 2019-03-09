7153424bf18516b1913de7677236e3c5984be7c4
Muhtasim Mahir <muhtasimmahir@Muhtasims-MBP.fios-router.home>
2019-03-09 00:10:37 -0500

    Fibonacci & FLD

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,15 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+        int n = 40, num1 = 0, num2 = 1;
 
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(num1 + " , ");
+            int sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
 
     }
 }
--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,4 +1,5 @@
 package math.problems;
+import java.util.Arrays;
 
 public class FindLowestDifference {
     public static void main(String[] args) {
@@ -9,7 +10,29 @@ public class FindLowestDifference {
         */
         int [] array1 = {30,12,5,9,2,20,33,1};
         int [] array2 = {18,25,41,47,17,36,14,19};
+        int x = array1.length;
+        int y = array2.length;
+        System.out.println(" Lowest Difference between the cells is : "+ difference( array1, array2, x, y));
 
+    }
+
+    static int difference(int Array1[], int Array2[], int x, int y) {
+        Arrays.sort(Array1);
+        Arrays.sort(Array2);
+        int val1 = 0, val2 = 0;
+
+        int result = Integer.MAX_VALUE;
+
+        while (val1 < x && val2 < y)
+        {
+            if (Math.abs(Array1[val1] - Array2[val2]) < result)
+                result = Math.abs(Array1[val1] - Array2[val2]);
+            if (Array1[val1] < Array2[val2])
+                val1++;
+            else
+                val2++;
+        }
+        return result;
 
 
 
