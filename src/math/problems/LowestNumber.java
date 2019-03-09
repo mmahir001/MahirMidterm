1e9a79ccd057a4bc00311d571b0e7dafb33b51e9
Muhtasim Mahir <muhtasimmahir@Muhtasims-MBP.fios-router.home>
2019-03-09 01:14:15 -0500

    LowestNumber done

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -1,7 +1,6 @@
 package math.problems;
 
 import databases.ConnectToSqlDB;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +12,20 @@ public class LowestNumber {
 		 * Use one of the databases from mysql or mongodb to store and to retrieve.
 		 */
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
+		int size;
+		int val;
+		size = array.length;
 
-		//find lowest number from the array
+		for(int i = 0; i<size; i++ ){
+			for(int j = i+1; j<size; j++){
+				if(array[i]>array[j]){
+					val = array[i];
+					array[i] = array[j];
+					array[j] = val;
+				}
+			}
+		}
+		System.out.println("Smallest number in the array is:: "+array[0]);
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		List<String> lowestValue = new ArrayList<String>();
